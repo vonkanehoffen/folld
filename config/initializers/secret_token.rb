4a3307599af737972f52b63598c72fdff8f3c6f9
Kane Clover <kane@assure24.com>
2013-03-16 13:51:00 +0000

    vanilla rails 3.2.12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Folld::Application.config.secret_token = 'ac97a941534cf52cf22b75a1de78245a07ab54e4c7ca32ae5355c45d2cc27ac4483a439577c37f54574c4a8fb14df247af82962332695656ef8c1e515bf460da'
