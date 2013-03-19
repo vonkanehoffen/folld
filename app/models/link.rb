9170365d820ad4e75efadcc701a057f392fc58ba
Kane Clover <kane@assure24.com>
2013-03-19 21:33:11 +0000

    validate url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,14 @@
 class Link < ActiveRecord::Base
   attr_accessible :description, :private, :title, :uri, :tag_list
+  
+  validates_presence_of :title
+  
+  validates_each :uri do |record, attr, value|
+  	if !(value =~ URI::regexp)
+	  	record.errors.add(attr, 'Must be a valid URL')
+  	end
+  end
+  
   acts_as_taggable
   belongs_to :user
 end
