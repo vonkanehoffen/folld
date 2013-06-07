7b761fb8f8950eeb01485f85c59a14c467698544
Kane Clover <kane@assure24.com>
2013-06-07 09:12:48 +0100

    replace masonry plugin with custom js

--- app/assets/javascripts/interface.js
+++ app/assets/javascripts/interface.js
@@ -47,23 +47,21 @@ $(document).ready(function(){
         })
     }
 
-    // Setup masonry layout ---------------------------------------------------
+    // Setup tiles layout ---------------------------------------------------
 
-    $('#main.links_view').masonry({
-        itemSelector: '.masonry_item',
-        //gutterWidth: 20,
-        columnWidth: function( containerWidth ) {
-            return containerWidth / 3;
-        }
+    folld.tiles.init();
+    folld.tiles.resize();
+    $(window).resize(function(){
+        if(folld.tiles.col_count != folld.tiles.get_col_count()) folld.tiles.init();
+       folld.tiles.resize();
     });
-
+                   
     // Add non-UJS interface handlers -----------------------------------------
 
     $('a#add_new_link').on('click', function(){
         if($(this).hasClass('active')) {
             $(this).removeClass('active');
             $('form#new_link').remove();
-            reload_masonry();
             return false;
         }
     });
@@ -71,35 +69,90 @@ $(document).ready(function(){
     $(document).on('click', 'form.edit_link .cancel', function() {
         $(this).parents('.link').find('.show').show();
         $(this).parents('form').remove();
-        reload_masonry();
         return false;
     });
 
 })
 
-// Helpers --------------------------------------------------------------------
+var folld = {
+    is_url: function(url) {
+        // TODO: This doesn't validate URLs like: http://www.kvraudio.com/forum/viewtopic.php?p=5238905
+        // ....and might crash chrome?
+        // And this: http://www.google.com/fonts/#QuickUsePlace:quickUse/Family:
+        if(url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)) {
+            return true; 
+        } else { 
+            return false; 
+        }
+    },
 
-function is_url(url) {
-    // TODO: This doesn't validate URLs like: http://www.kvraudio.com/forum/viewtopic.php?p=5238905
-    // ....and might crash chrome?
-    // And this: http://www.google.com/fonts/#QuickUsePlace:quickUse/Family:
-    if(url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)) {
-        return true; } else { return false; }
+    has_transport: function(url) {
+        if(url.match(/^https?:\/\//)) {
+            return true; } else { return false; }
+    },
+
+    flash: function(type, msg) {
+        var flash = $('<div></div>');
+        flash.addClass(type);
+        flash.html(msg);
+        $('#flash').append(flash);
+        flash.delay(1500).fadeOut();
+    },
+
+    tiles: {
+        threshold: {
+            1: 500,
+            2: 650,
+            3: 900,
+            4: 1200,
+            5: 1600
+        },
+        cols: {},
+        get_col_count: function() {
+            // Find correct column count for page width
+            var col_count = 1;
+            var thresh = folld.tiles.threshold;
+            var width = $('.tiles_outer').width();
+            for(var num in thresh) {
+                if(width > thresh[num]) { 
+                    col_count = num 
+                }
+            }
+            
+            return col_count;
+        },
+        init: function() {
+            folld.tiles.col_count = folld.tiles.get_col_count();
+            col_count = folld.tiles.col_count;
+
+            // Create column containers
+            for(var i=0; i<col_count; i++) {
+                folld.tiles.cols[i] = $('<div class="tiles_col"><div class="inner"></div></div>');
+            }
+
+            // Move tiles into columns
+            var current = 0;
+            $('.tiles_item').each(function(i){
+                folld.tiles.cols[current].find('.inner').append($(this));
+                current++;
+                if(current>=col_count) { current = 0; }
+            });
+
+            // Attach columns to the DOM
+            $('.tiles_col').remove();
+            for(var i=0; i<col_count; i++) {
+                $('.tiles_inner').append(folld.tiles.cols[i]);
+            }
+
+        },
+        resize: function() {
+            var outer_width = $('.tiles_outer').width();
+            console.log("outer ",outer_width);
+            var col_width = Math.floor(outer_width/folld.tiles.col_count);
+
+            $('.tiles_inner').width(outer_width);
+            $('.tiles_col').width(col_width);
+        }
+    }
 }
 
-function has_transport(url) {
-    if(url.match(/^https?:\/\//)) {
-        return true; } else { return false; }
-}
-
-function flash(type, msg) {
-    var flash = $('<div></div>');
-    flash.addClass(type);
-    flash.html(msg);
-    $('#flash').append(flash);
-    flash.delay(1500).fadeOut();
-}
-
-function reload_masonry() {
-    $('#main').masonry('reload');
-}
